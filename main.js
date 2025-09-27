3ab0f32beb137742ea4d2e53caea4119f211e35b
DionelValera <Dionelvalera@gmail.com>
2025-09-27 01:04:27 -0400

    aplicaci{on de electron finalizada y funcional
    (no tome la oportunidad de poner mas comits en github, que tonto)

--- main.js
+++ main.js
@@ -4,22 +4,15 @@ const path = require('path');
 
 let mainWindow;
 
-/**
- * Crea la ventana principal de la aplicación.
- */
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1200, 
     height: 800, 
-    // === RESTAURADO: Usamos el marco de ventana nativo del SO ===
     frame: true, 
-    // Esto ya no es necesario, el OS maneja el título
-    // titleBarStyle: 'hidden', 
     
-    // Configuración de Ícono
-    icon: path.join(__dirname, 'icon.png'),
+    icon: path.join(__dirname, 'logo.png'),
     webPreferences: {
-      // Dejamos el preload por si lo necesitas en el futuro, pero estará vacío.
+      // Dejamos el preload por si lo necesitamos en el futuro, pero ahora está vacío.
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false, 
       contextIsolation: true 
@@ -47,8 +40,6 @@ function createWindow () {
     mainWindow = null;
   });
   
-  // No necesitamos IPC listeners para minimizar/maximizar/cerrar,
-  // ya que el marco nativo de Linux/Hyprland los maneja.
 }
 
 app.whenReady().then(() => {
